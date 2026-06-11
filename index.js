e1dff3da360f94d868fa148bb698e302dc0fa02d
ankitkumarbarik <barikankitkumar2@gmail.com>
2026-06-11 11:59:33 +0530

    ok

---
+++ index.js
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  return res.json({ message: "server is healthy ❤️" });
+});
+
+app.listen(PORT, () => console.log(`server is running on port ${PORT} 🟢`));
