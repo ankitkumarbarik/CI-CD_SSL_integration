76d596f19d82750e132c07e396371c03287aaac0
ankitkumarbarik <barikankitkumar2@gmail.com>
2026-06-11 12:22:49 +0530

    fix: update health check message to include emoji for clarity

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  return res.json({ message: "server is healthy" });
+  return res.json({ message: "server is healthy 🐳" });
 });
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT} 🟢`));
